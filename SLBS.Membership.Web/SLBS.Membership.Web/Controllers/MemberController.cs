6037060dc15d0debf08d73dffa4f8e1a891f08f4
unknown <uchitha.r@gmail.com>
2016-04-27 00:08:29 +0800

    init checkin

---
+++ SLBS.Membership.Web/SLBS.Membership.Web/Controllers/MemberController.cs
@@ -0,0 +1,75 @@
+﻿using OfficeOpenXml;
+using SLBS.Membership.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SLBS.Membership.Web.Controllers
+{
+    public class MemberController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Load()
+        {
+            var list = new List<Member>();
+
+            if (Request.Files.Count > 0)
+            {
+                var file = Request.Files[0];
+              
+
+                if (file != null && file.ContentLength > 0)
+                {
+                    if (Path.GetExtension(file.FileName) == ".xlsx")
+                    {
+                        list = ProcessMembershipFile(file);
+                    }
+                }
+            }
+
+            return Show(list);
+        }
+
+        public ActionResult Show(List<Member> list)
+        {
+            return View("Show",list);
+        }
+
+        private List<Member> ProcessMembershipFile(HttpPostedFileBase file)
+        {
+            using (var package = new ExcelPackage(file.InputStream))
+            {
+                // get the first worksheet in the workbook
+                ExcelWorksheet worksheet = package.Workbook.Worksheets["MEM_BF"];
+                int col = 12;
+                var list = new List<Member>();
+                for (int row = 6; worksheet.Cells[row, col].Value != null; row++)
+                {
+                    var email = worksheet.Cells[row, col - 3].Value != null ? worksheet.Cells[row, col - 3].Value.ToString() : string.Empty;
+                    var memberNo = worksheet.Cells[row, col - 2].Value.ToString();
+                    var paymentStatus = worksheet.Cells[row, col].Value.ToString();
+                    if (paymentStatus == "Pay")
+                    {
+                        list.Add(new Member
+                        {
+                            MemberNo = memberNo,
+                            Email = email,
+                            PaymentStatus = paymentStatus
+                        });
+                    }
+                   
+                }
+                return list;
+            } 
+
+        }
+    }
+}
---
+++ SLBS.Membership.Web/SLBS.Membership.Web/Models/Member.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SLBS.Membership.Web.Models
+{
+    public class Member
+    {
+        public string MemberNo { get; set; }
+        public string Email { get; set; }
+        public string PaymentStatus { get; set; }
+    }
+}
