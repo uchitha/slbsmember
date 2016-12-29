4fcc04a29335b2a43f49263b24dcd99534cd6c92
Chris Ranasinghe <chris.ranasinghe1@bhpbilliton.com>
2016-12-29 08:42:59 +0800

    Update initializer with real data

--- SLBS.Membership.Web/SLBS.Membership.Domain/SlsbsInitializer.cs
+++ SLBS.Membership.Web/SLBS.Membership.Domain/SlsbsInitializer.cs
@@ -10,9 +10,9 @@ namespace SLBS.Membership.Domain
         {
             var members = new List<Membership>()
             {
-                new Membership {MembershipId = 1, MembershipNumber = "M001", ContactName = "Mr. & Mrs. M01", PaidUpTo = new DateTime(2016,1,1)},
-                new Membership {MembershipId = 2, MembershipNumber = "M002", ContactName = "Mr. & Mrs. M02", PaidUpTo = new DateTime(2016,4,1)},
-                new Membership {MembershipId = 3, MembershipNumber = "M003", ContactName = "Mr. & Mrs. M03", PaidUpTo = new DateTime(2016,12,1)},
+                new Membership {MembershipId = 1, MembershipNumber = "W041", ContactName = "Chandana Weerasekera Family", PaidUpTo = new DateTime(2016,1,1)},
+                new Membership {MembershipId = 2, MembershipNumber = "G022", ContactName = "Pramodh Gunawardena Family", PaidUpTo = new DateTime(2016,4,1)},
+                new Membership {MembershipId = 3, MembershipNumber = "R027", ContactName = "Nilakshi & Uchitha", PaidUpTo = new DateTime(2015,12,1)},
             };
 
             members.ForEach(m => context.Memberships.Add(m));
@@ -20,9 +20,9 @@ namespace SLBS.Membership.Domain
 
             var mothers = new List<Adult>()
             {
-                new Adult() {AdultId = 1, MembershipId = 1, FullName = "Mom 1", Role = MembershipRole.Mother, Email = "mom1@gmail.com"},
-                new Adult() {AdultId = 2, MembershipId = 2, FullName = "Mom 2", Role = MembershipRole.Mother, Email = "mom2@gmail.com"},
-                new Adult() {AdultId = 3, MembershipId = 3, FullName = "Mom 3", Role = MembershipRole.Mother, Email = "mom3@gmail.com"},
+                new Adult() {AdultId = 1, MembershipId = 1, FullName = "Varuni Weerasekara", Role = MembershipRole.Mother, Email = "cargillsvaruni@yahoo.co.uk"},
+                new Adult() {AdultId = 2, MembershipId = 2, FullName = "Wasantha Gunawardhana", Role = MembershipRole.Mother, Email = "w.gunawardhana@gmail.com"},
+                new Adult() {AdultId = 3, MembershipId = 3, FullName = "Nilakshi Abeysinghe", Role = MembershipRole.Mother, Email = "nilakshia@live.com"},
             };
 
             mothers.ForEach(m => context.Adults.Add(m));
@@ -30,9 +30,9 @@ namespace SLBS.Membership.Domain
 
             var fathers = new List<Adult>()
             {
-                new Adult() {AdultId = 4, MembershipId = 1, FullName = "Dad 1", Role = MembershipRole.Father, Email = "dad1@gmail.com"},
-                new Adult() {AdultId = 5, MembershipId = 2, FullName = "Dad 2", Role = MembershipRole.Father, Email = "dad2@gmail.com"},
-                new Adult() {AdultId = 6, MembershipId = 3, FullName = "Dad 3", Role = MembershipRole.Father, Email = "dad3@gmail.com"},
+                new Adult() {AdultId = 4, MembershipId = 1, FullName = "Chandana Weerasekera", Role = MembershipRole.Father, Email = "Chaweea8@yahoo.co.uk"},
+                new Adult() {AdultId = 5, MembershipId = 2, FullName = "Pramodh Gunwardhana", Role = MembershipRole.Father, Email = "p.gunawardhana@curtin.com.au"},
+                new Adult() {AdultId = 6, MembershipId = 3, FullName = "Uchitha Ranasinghe", Role = MembershipRole.Father, Email = "uchitha.r@gmail.com"},
             };
 
             fathers.ForEach(m => context.Adults.Add(m));
@@ -40,12 +40,10 @@ namespace SLBS.Membership.Domain
 
             var kids = new List<Child>()
             {
-                new Child(){ChildId = 1, MembershipId = 1, FullName = "Child1 M001",AmbulanceCover = true,MediaConsent = false,ClassLevel = ClassLevelEnum.Level1},
-                new Child(){ChildId = 2, MembershipId = 1, FullName = "Child2 M001",AmbulanceCover = true,MediaConsent = false,ClassLevel = ClassLevelEnum.Level2},
-                new Child(){ChildId = 3, MembershipId = 2, FullName = "Child1 M002",AmbulanceCover = false,MediaConsent = true,ClassLevel = ClassLevelEnum.Level2},
-                new Child(){ChildId = 4, MembershipId = 2, FullName = "Child2 M002",AmbulanceCover = false,MediaConsent = true,ClassLevel = ClassLevelEnum.Level3},
-                new Child(){ChildId = 5, MembershipId = 2, FullName = "Child3 M002",AmbulanceCover = false,MediaConsent = true,ClassLevel = ClassLevelEnum.Level5},
-                new Child(){ChildId = 6, MembershipId = 3, FullName = "Child1 M003",AmbulanceCover = true,MediaConsent = true,ClassLevel = ClassLevelEnum.Level1}
+                new Child(){ChildId = 1, MembershipId = 1, FullName = "Rashini Weerasekara",AmbulanceCover = true,MediaConsent = false,ClassLevel = ClassLevelEnum.Level6},
+                new Child(){ChildId = 2, MembershipId = 2, FullName = "Nisani Gunawardhana",AmbulanceCover = false,MediaConsent = true,ClassLevel = ClassLevelEnum.Level2},
+                 new Child(){ChildId = 3, MembershipId = 2, FullName = "Binithi Gunawardhana",AmbulanceCover = false,MediaConsent = true,ClassLevel = ClassLevelEnum.Level3},
+                new Child(){ChildId = 4, MembershipId = 3, FullName = "Thumindu Ranasinghe",AmbulanceCover = true,MediaConsent = true,ClassLevel = ClassLevelEnum.Level1}
             };
 
             kids.ForEach(k => context.Children.Add(k));
